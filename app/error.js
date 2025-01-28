132510818a11bae59a9f8dcda7ccd10f2c8836d8
msisaif <msi313.bd@gmail.com>
2025-01-28 15:01:47 +0600

    enhance error handling UI with improved styling and layout

--- app/error.js
+++ app/error.js
@@ -9,16 +9,40 @@ export default function Error({ error, reset }) {
     }, [error]);
 
     return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '32px 16px',
+            }}
+        >
+            <div
+                style={{
+                    textAlign: 'center',
+                    border: '1px solid #cccc',
+                    padding: '32px 8px',
+                    background: '#fff',
+                    borderRadius: '8px',
+                    minWidth: '320px',
+                }}
             >
-                Try again
-            </button>
+                <h1 style={{ fontSize: '24px', color: '#f20' }}>Something went wrong!</h1>
+                <br />
+                <button
+                    type="button"
+                    style={{
+                        border: '1px solid #cccc',
+                        padding: '8px 32px',
+                        background: '#456',
+                        borderRadius: '8px',
+                        color: '#fff',
+                    }}
+                    onClick={() => reset()}
+                >
+                    Try again
+                </button>
+            </div>
         </div>
     );
 }
